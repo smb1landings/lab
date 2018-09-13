5bce2e32d6323f9e2a6890e7b10c540b4c503e56
Administrador De Dominio <admin.sistemas@humancvp.mx>
2018-09-13 15:57:55 -0500

    landingLaboratorios

---
+++ laboratorios/js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by julio.ramos on 31/08/2018.
+ */
+setTimeout(
+    function()
+    {
+        $(document).ready(function(){
+            //$.when($("div.cover").fadeOut()).then($("body").css("background-color","black"));
+            $.when($("div.cover").fadeOut());
+        });
+    }, 500);
